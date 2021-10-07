4f77ca02f6f4045f106034678509f35f8a39fca6
Barbara Pitt <cyberleftagitprop@gmail.com>
2021-10-07 15:28:17 -0500

    removed controller wide '/' request mapping from main.java

--- src/main/java/org/cabalchan/cabalchan/controllers/Main.java
+++ src/main/java/org/cabalchan/cabalchan/controllers/Main.java
@@ -30,7 +30,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
