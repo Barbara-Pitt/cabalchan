f11a0c5c3f5046fa55e9fdcd4dd30d31052917af
Barbara Pitt <cyberleftagitprop@gmail.com>
2021-10-09 21:51:44 -0500

    added missing controller annotation to post history mod controller

--- src/main/java/org/cabalchan/cabalchan/controllers/ModPostHistoryController.java
+++ src/main/java/org/cabalchan/cabalchan/controllers/ModPostHistoryController.java
@@ -3,10 +3,12 @@ package org.cabalchan.cabalchan.controllers;
 import org.cabalchan.cabalchan.repositories.EntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+@Controller
 public class ModPostHistoryController {
 
     @Autowired
