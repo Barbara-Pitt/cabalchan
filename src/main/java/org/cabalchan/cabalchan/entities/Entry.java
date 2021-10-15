2c9dec8ed876351263427cdf129f61bcbb34058f
Barbara Pitt <cyberleftagitprop@gmail.com>
2021-10-14 21:24:45 -1000

    removed filter from entry entity

--- src/main/java/org/cabalchan/cabalchan/entities/Entry.java
+++ src/main/java/org/cabalchan/cabalchan/entities/Entry.java
@@ -45,10 +45,6 @@ public class Entry {
     @JoinColumn(name="category_id")
     private Category category;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name="filter_id")
-    private Filter filter;
-
     @OneToOne(mappedBy = "entry",fetch = FetchType.LAZY)
     private Attachment attachment;
 
